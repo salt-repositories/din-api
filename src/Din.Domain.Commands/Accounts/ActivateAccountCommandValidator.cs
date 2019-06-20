e893d30024fefdeef889c2a256852920c25b03a3
Dane Naebers <danenaebers@gmail.com>
2019-06-21 00:01:43 +0200

    add account acivation command validator

---
+++ src/Din.Domain.Commands/Accounts/ActivateAccountCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Din.Domain.Commands.Accounts
+{
+    public class ActivateAccountCommandValidator : AbstractValidator<ActivateAccountCommand>
+    {
+        public ActivateAccountCommandValidator()
+        {
+            RuleFor(cmd => cmd.Code)
+                .NotEmpty().WithMessage("The code cannot be empty")
+                .Length(30).WithMessage("The code is exactly thirty characters long");
+        }
+    }
+}
