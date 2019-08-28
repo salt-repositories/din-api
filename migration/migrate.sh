3147af7796477cf141c3f7b8e2810e857c56cc96
Dane Naebers <danenaebers@gmail.com>
2019-08-28 23:37:07 +0200

    add deploy and migration scripts

---
+++ migration/migrate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting migration"
+
+migration_image_name = $1
+secret=$2
+assembly="/app/Din.Infrastructure.Migrations.dll"
+
+docker run --rm $migration_image_name dotnet fm migrate -p mysql -c $secret -a $assembly
