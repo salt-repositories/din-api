aa7efcc44af358c868f353ab1cc6aefaa9031ae2
Dane Naebers <danenaebers@gmail.com>
2019-08-29 15:24:42 +0200

    add deploy and migration scripts (#22)

---
+++ deploy/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $1 = "Production"]; then
+    echo "Starting production deployment"
+    source deploy/production.vars
+else
+    echo "Starting nightly deployment"
+    source deploy/nightly.vars
+fi
+
+id=$2
+secret=$3
+image_name=$4
+
+docker stop $name || true && docker rm $name || true
+docker run -d --name $name -p $port:80 -e VAULT_URL=$vault_url -e VAULT_ID=$id -e VAULT_SECRET=$secret -e ASPNETCORE_ENVIRONMENT=$environment $image_name
+
+echo "Finished deployment"
