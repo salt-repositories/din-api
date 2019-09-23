8b7eb1ef254b882e69452e42d5c50b90dc332f64
Dane Naebers <danenaebers@gmail.com>
2019-09-23 23:18:35 +0200

    allow all origins for now

--- src/Din.Application.WebAPI/Startup.cs
+++ src/Din.Application.WebAPI/Startup.cs
@@ -34,6 +34,13 @@ namespace Din.Application.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("Default", builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
             services.RegisterMvcComponents();
             services.RegisterAuthentication(Configuration);
             services.RegisterSwagger();
@@ -52,6 +59,8 @@ namespace Din.Application.WebAPI
 
             InitializeContainer(app, env);
 
+            app.UseCors("Default");
+            
             app.UseAuthentication();
             app.UseMvc();
             app.UseSwagger();
