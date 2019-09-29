3bca0b269ad7449a619f0f708d4cfec9f98be1fb
Dane Naebers <danenaebers@gmail.com>
2019-09-29 16:04:09 +0200

    update content store query implementation

--- src/Din.Domain/Stores/Interfaces/IContentStore.cs
+++ src/Din.Domain/Stores/Interfaces/IContentStore.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
+using Din.Domain.Models.Querying;
 
 namespace Din.Domain.Stores.Interfaces
 {
     public interface IContentStore<T>
     {
         ICollection<T> Content { get; }
-
         bool ShouldUpdate();
-        ICollection<T> GetAll();
+        ICollection<T> GetAll(QueryParameters<T> queryParameters, string title);
         T GetOneById(int id);
-        ICollection<T> GetMultipleByTitle(string title);
         void Set(ICollection<T> contentCollection);
         void AddOne(T content);
+        int Count(string title);
     }
 }
