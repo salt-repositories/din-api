dd3b464d8ffcc83dc6be4e426469dfb7a4d22f4c
Dane Naebers <danenaebers@gmail.com>
2020-04-04 16:37:41 +0200

    package updates and content store refresh task (#37)
    * code cleanup
    
    * add content store refresh task

---
+++ src/Din.Domain/BackgroundTasks/Concrete/ContentStoreRefresher.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Din.Domain.BackgroundTasks.Interfaces;
+using Din.Domain.Clients.Radarr.Interfaces;
+using Din.Domain.Clients.Radarr.Responses;
+using Din.Domain.Clients.Sonarr.Interfaces;
+using Din.Domain.Clients.Sonarr.Responses;
+using Din.Domain.Stores.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Din.Domain.BackgroundTasks.Concrete
+{
+    public class ContentStoreRefresher : IBackgroundTask
+    {
+        private readonly ILogger<ContentStoreRefresher> _logger;
+        private readonly IContentStore<RadarrMovie> _movieStore;
+        private readonly IContentStore<SonarrTvShow> _tvShowStore;
+        private readonly IRadarrClient _radarrClient;
+        private readonly ISonarrClient _sonarrClient;
+
+        public ContentStoreRefresher(ILogger<ContentStoreRefresher> logger, IContentStore<RadarrMovie> movieStore, IContentStore<SonarrTvShow> tvShowStore, IRadarrClient radarrClient, ISonarrClient sonarrClient)
+        {
+            _logger = logger;
+            _movieStore = movieStore;
+            _tvShowStore = tvShowStore;
+            _radarrClient = radarrClient;
+            _sonarrClient = sonarrClient;
+        }
+
+        public async Task Execute(CancellationToken cancellationToken)
+        {
+            if (_movieStore.ShouldUpdate())
+            {
+                _logger.LogInformation("Refreshing movie store");
+                _movieStore.Set((await _radarrClient.GetMoviesAsync(cancellationToken)).ToList());
+            }
+
+            if (_tvShowStore.ShouldUpdate())
+            {
+                _logger.LogInformation("Refreshing tv show store");
+                _tvShowStore.Set((await _sonarrClient.GetTvShowsAsync(cancellationToken)).ToList());
+            }
+        }
+    }
+}
